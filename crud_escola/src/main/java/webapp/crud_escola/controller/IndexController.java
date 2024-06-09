ad7e0ff539e0f8d6c2484024b934296f93da6b8e
EllysonRPT <reptile1284@gmail.com>
2024-06-09 11:45:34 -0300

    09/06

--- crud_escola/src/main/java/webapp/crud_escola/controller/IndexController.java
+++ crud_escola/src/main/java/webapp/crud_escola/controller/IndexController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
+  
 @Controller
 public class IndexController {
-
+//
     // método
     @GetMapping("/")
     public ModelAndView abrirIndex() {
